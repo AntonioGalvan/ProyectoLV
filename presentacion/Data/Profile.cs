7070f0f780ff3e06e3f6aa063525c53d61ffc449
AntonioGalvan <antonio.uriza97@gmail.com>
2020-04-23 02:39:42 -0500

    Especificaciones de propiedades profile

--- presentacion/Data/Profile.cs
+++ presentacion/Data/Profile.cs
@@ -1,14 +1,28 @@
 ﻿namespace presentacion.Data
 {
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     public class Profile :IEntity
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(30)]
         public string LastName { get; set; }
+        [Required]
         public int Age { get; set; }
+        [Required]
+        [StringLength(30)]
         public string UserName { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Password { get; set; }
+        [Required]
+        [StringLength(25)]
         public string City { get; set; }
 
         public Contact Contact { get; set; }
---
+++ presentacion/Migrations/202004230620078_modificacion1.cs
@@ -0,0 +1,30 @@
+﻿namespace presentacion.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class modificacion1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Advices", "text", c => c.String());
+            AlterColumn("dbo.Profiles", "Name", c => c.String(nullable: false, maxLength: 20));
+            AlterColumn("dbo.Profiles", "LastName", c => c.String(nullable: false, maxLength: 30));
+            AlterColumn("dbo.Profiles", "UserName", c => c.String(nullable: false, maxLength: 30));
+            AlterColumn("dbo.Profiles", "Password", c => c.String(nullable: false, maxLength: 30));
+            AlterColumn("dbo.Profiles", "City", c => c.String(nullable: false, maxLength: 25));
+            DropColumn("dbo.Advices", "Number");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Advices", "Number", c => c.Int(nullable: false));
+            AlterColumn("dbo.Profiles", "City", c => c.String());
+            AlterColumn("dbo.Profiles", "Password", c => c.String());
+            AlterColumn("dbo.Profiles", "UserName", c => c.String());
+            AlterColumn("dbo.Profiles", "LastName", c => c.String());
+            AlterColumn("dbo.Profiles", "Name", c => c.String());
+            DropColumn("dbo.Advices", "text");
+        }
+    }
+}
