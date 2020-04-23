c2b2352d9b04973e2af82a9a34da823669a33dfe
manuel23arredondo <arredondo23manuel@gmail.com>
2020-04-23 03:28:22 -0500

    Cambio de DataContext

--- presentacion/DataContext.cs
+++ presentacion/DataContext.cs
@@ -4,11 +4,13 @@
     using System.Data.Entity;
     public class DataContext : DbContext
     {
-        public DbSet<Question> Advices { get; set; }
-        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Question> Questions { get; set; }
         public DbSet<Expert> Experts { get; set; }
-        public DbSet<User> Profiles { get; set; }
-        public DbSet<Test> Updates { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<TestQuestion> TestQuestions { get; set; }
+        public DbSet<Response> Responses { get; set; }
         public DataContext() : base("name=Connection")
         {
 
