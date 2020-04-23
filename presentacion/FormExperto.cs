6d8d8b765a3b41bc2795b45ae7713bb788002846
manuel23arredondo <arredondo23manuel@gmail.com>
2020-04-23 01:49:19 -0500

    Cambio a MetroFramework, la forma "FormExperto"

--- presentacion/FormExperto.cs
+++ presentacion/FormExperto.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace presentacion
 {
-    public partial class FormExperto : Form
-    {
+    public partial class FormExperto : MetroFramework.Forms.MetroForm
+    { 
         public FormExperto()
         {
             InitializeComponent();
